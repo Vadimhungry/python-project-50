00f3d4805fed4f6b6a5ac455df768bd5cd490650
VadimK <yaralavadim@gmail.com>
2023-08-04 13:05:19 +0300

    parse_data.py refactor

--- gendiff/parse_data.py
+++ gendiff/parse_data.py
@@ -4,30 +4,17 @@ from yaml.loader import SafeLoader
 import os
 
 
-def get_file_extension(file):
-    _, file_extension = os.path.splitext(file)
-    return file_extension
-
-
 def get_file_data(file):
-    format = get_file_extension(file)
-
-    match format:
-        case '.json':
-            return parse_json(file)
-        case '.yml' | '.yaml':
-            return parse_yaml(file)
-        case _:
-            print("Unknown extension! I can't parse it!")
+    _, file_extension = os.path.splitext(file)
+    return parse(file, file_extension)
 
 
-def parse_json(file):
-    with open(file) as data_file:
-        data = json.load(data_file)
-    return data
-
-
-def parse_yaml(file):
-    with open(file) as data_file:
-        data = yaml.load(data_file, Loader=SafeLoader)
-    return data
+def parse(content, format):
+    with open(content) as data_file:
+        match format:
+            case '.json':
+                return json.load(data_file)
+            case '.yml' | '.yaml':
+                return yaml.load(data_file, Loader=SafeLoader)
+            case _:
+                print("Unknown extension! I can't parse it!")
