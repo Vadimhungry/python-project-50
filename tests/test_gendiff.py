d61dc102a59335e08a7c8b861989af1a905289ab
VadimK <yaralavadim@gmail.com>
2023-08-04 11:10:31 +0300

    files as fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,23 +1,21 @@
 import pytest
 
 from gendiff.scripts.get_diff import generate_diff
-from gendiff.formatters.stylish import stylish
-from gendiff.formatters.plain import plain
 
 
 @pytest.fixture
-def data1():
-    return {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+def plain1():
+    return '/Users/Number1/PycharmProjects/python-project-50/tests/fixtures/plain1.json'
 
 
 @pytest.fixture
-def data2():
-    return {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
+def plain2():
+    return '/Users/Number1/PycharmProjects/python-project-50/tests/fixtures/plain2.json'
 
 
-def test_make_plaindiff(data1, data2):
-    assert stylish(generate_diff(data1, data2)) == '{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+def test_stylish_formatter_plain_files(plain1, plain2):
+    assert generate_diff(plain1, plain2) == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
 
 
-def test_plain_formatter(data1, data2):
-    assert plain(generate_diff(data1, data2)) == "Property 'follow' was removed\nProperty 'proxy' was removed\nProperty 'timeout' was updated. From 50 to '20'\nProperty 'verbose' was added with value: 'True' \n"
+def test_plain_formatter_plain_files(plain1, plain2):
+    assert generate_diff(plain1, plain2, formatter='plain') == "Property 'follow' was removed\nProperty 'proxy' was removed\nProperty 'timeout' was updated. From 50 to 20\nProperty 'verbose' was added with value: true"
