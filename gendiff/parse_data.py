8c0267e7b663aa83574920ce7acac3d08786b822
VadimK <yaralavadim@gmail.com>
2023-08-10 13:38:23 +0300

    refaktor parse_data.py

--- gendiff/parse_data.py
+++ gendiff/parse_data.py
@@ -6,15 +6,16 @@ import os
 
 def get_file_data(file):
     _, file_extension = os.path.splitext(file)
-    return parse(file, file_extension)
+    content = open(file, 'r')
+    return parse(content, file_extension)
 
 
 def parse(content, format):
-    with open(content) as data_file:
-        match format:
-            case '.json':
-                return json.load(data_file)
-            case '.yml' | '.yaml':
-                return yaml.load(data_file, Loader=SafeLoader)
-            case _:
-                print("Unknown extension! I can't parse it!")
+
+    match format:
+        case '.json':
+            return json.load(content)
+        case '.yml' | '.yaml':
+            return yaml.load(content, Loader=SafeLoader)
+        case _:
+            print("Unknown extension! I can't parse it!")
