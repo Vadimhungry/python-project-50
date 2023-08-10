5e6f8ce50ce85dc1c9184d71e41217e498f45267
VadimK <yaralavadim@gmail.com>
2023-08-10 13:19:24 +0300

    refaktor build_diff_tree

--- gendiff/get_diff.py
+++ gendiff/get_diff.py
@@ -8,82 +8,70 @@ def generate_diff(file1, file2, formatter='stylish'):
 
     data1 = get_file_data(file1)
     data2 = get_file_data(file2)
+    diff = build_diff_tree(data1, data2)
 
-    def inner(data1, data2, level=1, path=''):
+    return format_diff(diff, formatter)
 
-        diff = []
 
-        for key in sorted(list({**data1, **data2}.keys())):
+def build_diff_tree(data1, data2, level=1, path=''):
+    diff = []
 
-            if key not in data2:
+    for key in sorted(list({**data1, **data2}.keys())):
+
+        if key not in data2:
+            diff.append({
+                'key': key,
+                'action': 'deleted',
+                'file_1': data1[key],
+                'level': level,
+                'path': path
+
+            })
+
+        elif key not in data1:
+            diff.append({
+                'key': key,
+                'action': 'added',
+                'file_2': data2[key],
+                'level': level,
+                'path': path
+            })
+
+        elif key in data1 and key in data2:
+
+            if isinstance(data1[key], dict) \
+                    and isinstance(data2[key], dict):
                 diff.append({
                     'key': key,
-                    'action': 'deleted',
-                    'file_1': data1[key],
-                    # 'file_2': None,
+                    'action': 'nested',
+                    'children': build_diff_tree(
+                        data1[key],
+                        data2[key],
+                        level + 1,
+                        path + str(key) + '.'),
                     'level': level,
-                    'path': path
-
                 })
 
-            elif key not in data1:
+            elif data1[key] != data2[key]:
                 diff.append({
                     'key': key,
-                    'action': 'added',
-                    # 'file_1': None,
+                    'action': 'updated',
+                    'file_1': data1[key],
                     'file_2': data2[key],
                     'level': level,
                     'path': path
                 })
 
-            elif key in data1 and key in data2:
-
-                if isinstance(data1[key], dict) \
-                        and isinstance(data2[key], dict):
-                    diff.append({
-                        'key': key,
-                        'action': 'nested',
-                        'children': inner(
-                            data1[key],
-                            data2[key],
-                            level + 1,
-                            path + str(key) + '.'),
-                        'level': level,
-                    })
-
-                elif data1[key] != data2[key]:
-                    diff.append({
-                        'key': key,
-                        'action': 'updated',
-                        'file_1': data1[key],
-                        'file_2': data2[key],
-                        'level': level,
-                        'path': path
-                    })
-
-                elif data1[key] == data2[key]:
-                    diff.append({
-                        'key': key,
-                        'action': 'unchanged',
-                        'file_1': data1[key],
-                        'file_2': data2[key],
-                        'level': level,
-                        'path': path
-                    })
-        return diff
-
-    diff = inner(data1, data2)
-    # print(diff)
-    return format_diff(diff, formatter)
-
-
-# def parse_val(val):
-#     if isinstance(val, bool):
-#         return str(val).lower()
-#     if val is None:
-#         return 'null'
-#     else:
-#         return val
+            elif data1[key] == data2[key]:
+                diff.append({
+                    'key': key,
+                    'action': 'unchanged',
+                    'file_1': data1[key],
+                    'file_2': data2[key],
+                    'level': level,
+                    'path': path
+                })
+    return diff
 
 
 def format_diff(diff, formatter):
