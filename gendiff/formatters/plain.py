a681b3b7269ff5041a9cb5666eee42324914de57
VadimK <yaralavadim@gmail.com>
2023-08-11 14:08:02 +0300

    plain.py refactor

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -3,33 +3,39 @@ def plain(diff):
 
 
 def plainize(diff):
-    result = ''
-    for item in diff:
-        match item['action']:
 
-            case 'nested':
-                result += plainize(item['children'])
+    def inner(diff, path=''):
+        result = ''
+        for item in diff:
+            match item['action']:
 
-            case 'added':
-                result += 'Property ' + "'"
-                result += item['path'] + item['key'] + "'"
-                result += ' was added with value: '
-                result += format_val(item['new_value']) + '\n'
+                case 'nested':
+                    result += inner(
+                        item['children'],
+                        path + str(item['key']) + '.'
+                    )
 
-            case 'deleted':
-                result += 'Property ' + "'"
-                result += item['path'] + item['key'] + "'"
-                result += ' was removed\n'
+                case 'added':
+                    result += 'Property ' + "'"
+                    result += path + item['key'] + "'"
+                    result += ' was added with value: '
+                    result += format_val(item['new_value']) + '\n'
 
-            case 'updated':
-                result += 'Property ' + "'"
-                result += item['path'] + item['key'] + "'"
-                result += ' was updated. From '
-                result += format_val(item['old_value'])
-                result += ' to '
-                result += format_val(item['new_value']) + '\n'
+                case 'deleted':
+                    result += 'Property ' + "'"
+                    result += path + item['key'] + "'"
+                    result += ' was removed\n'
 
-    return result
+                case 'updated':
+                    result += 'Property ' + "'"
+                    result += path + item['key'] + "'"
+                    result += ' was updated. From '
+                    result += format_val(item['old_value'])
+                    result += ' to '
+                    result += format_val(item['new_value']) + '\n'
+
+        return result
+    return inner(diff)
 
 
 def format_val(val):
--- gendiff/get_diff.py
+++ gendiff/get_diff.py
@@ -7,7 +7,7 @@ def generate_diff(file1, file2, formatter='stylish'):
     data1 = get_file_data(file1)
     data2 = get_file_data(file2)
     diff = build_diff_tree(data1, data2)
-
+    print(diff)
     return format_diff(diff, formatter)
 
 
@@ -23,7 +23,6 @@ def build_diff_tree(data1, data2, level=1, path=''):
                 'old_value': data1[key],
                 'level': level,
                 'path': path
-
             })
 
         elif key not in data1:
