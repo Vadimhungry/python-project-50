52bb71233c2979eaffdadb47b05b79d628773dde
VadimK <yaralavadim@gmail.com>
2023-08-17 13:11:32 +0300

    test-coverage fix

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -4,76 +4,85 @@ def stylish(diff, replacer=' ', spacesCount=1):
 
 def stylize(diff, replacer=' ', spacesCount=1, level=1):
 
-    result = ''
+    result = []
 
     for item in diff:
 
         prekey_replacer = replacer * (spacesCount * level * 4 - 2)
         prebracket_replacer = replacer * (spacesCount * level * 4)
 
-        result += '\n'
+        result.append('\n') 
 
         match item['action']:
 
             case 'nested':
-                result += prekey_replacer + '  '
-                result += item['key'] + ': {'
-                result += stylize(
-                    item['children'],
-                    replacer,
-                    spacesCount,
-                    level + 1
+                result.append(f'{prekey_replacer}  {item["key"]}: {"{"}')
+                result.append(
+                    stylize(
+                        item['children'],
+                        replacer,
+                        spacesCount,
+                        level + 1
+                    )
                 )
-                result += '\n' + prebracket_replacer + '}'
+                result.append(f'\n{prebracket_replacer}{"}"}')
 
             case 'added':
-                result += prekey_replacer + '+ ' + item['key'] + ': '
-                result += to_str(
-                    item['new_value'],
-                    replacer,
-                    spacesCount,
-                    level
+                result.append(prekey_replacer + '+ ' + item['key'] + ': ')
+                result.append( 
+                    to_str(
+                        item['new_value'],
+                        replacer,
+                        spacesCount,
+                        level
+                    )
                 )
 
             case 'deleted':
-                result += prekey_replacer + '- ' + item['key'] + ': '
-                result += to_str(
-                    item['old_value'],
-                    replacer,
-                    spacesCount,
-                    level
+                result.append(prekey_replacer + '- ' + item['key'] + ': ')
+                result.append(
+                    to_str(
+                        item['old_value'],
+                        replacer,
+                        spacesCount,
+                        level
+                    )
                 )
-
+                
             case 'unchanged':
-                result += prekey_replacer + '  ' + item['key'] + ': '
-                result += to_str(
-                    item['old_value'],
-                    replacer,
-                    spacesCount,
-                    level
+                result.append(prekey_replacer + '  ' + item['key'] + ': ')
+                result.append(
+                    to_str(
+                        item['old_value'],
+                        replacer,
+                        spacesCount,
+                        level
+                    )
                 )
-
             case 'updated':
-                result += prekey_replacer + '- ' + item['key'] + ': '
-                result += to_str(
-                    item['old_value'],
-                    replacer,
-                    spacesCount,
-                    level
+                result.append(prekey_replacer + '- ' + item['key'] + ': ')
+                result.append(
+                    to_str(
+                        item['old_value'],
+                        replacer,
+                        spacesCount,
+                        level
+                    )
                 )
+                result.append('\n')
 
-                result += '\n'
-
-                result += prekey_replacer + '+ ' + item['key'] + ': '
-                result += to_str(
-                    item['new_value'],
-                    replacer,
-                    spacesCount,
-                    level
+                result.append(prekey_replacer + '+ ' + item['key'] + ': ')
+                result.append(
+                    to_str(
+                        item['new_value'],
+                        replacer,
+                        spacesCount,
+                        level
+                    )
                 )
                 # result += '\n' + prebracket_replacer + 'AAA'
 
-    return result
+    return ''.join(result)
 
 
 def to_str(argument, replacer=' ', spacesCount=1, level=0):
