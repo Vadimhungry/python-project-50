f2996f5dbf516507b4768f38cafb1ff60e4bbb17
VadimK <yaralavadim@gmail.com>
2023-08-18 11:33:14 +0300

    stylish.py refactor

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,114 +1,77 @@
-def stylish(diff, replacer=' ', spacesCount=1):
-    return stylize(diff, replacer, spacesCount)
+def stylish(diff, replacer=' ', spaces_count=1):
+    return stylize(diff, replacer, spaces_count)
 
 
-def stylize(diff, replacer=' ', spacesCount=1, level=1):
+def stylize(diff, replacer=' ', spaces_count=1, level=1):
 
     result = []
-    if level == 1:
-        result.append('{')
 
     for item in diff:
 
-        prekey_replacer = replacer * (spacesCount * level * 4 - 2)
-        prebracket_replacer = replacer * (spacesCount * level * 4)
-
-        result.append('\n')
+        prekey_replacer = replacer * (spaces_count * level * 4 - 2)
+        prebracket_replacer = replacer * (spaces_count * level * 4)
 
         match item['action']:
 
             case 'nested':
-                result.append(f'{prekey_replacer}  {item["key"]}: {"{"}')
                 result.append(
-                    stylize(
-                        item['children'],
-                        replacer,
-                        spacesCount,
-                        level + 1
-                    )
+                    f'{prekey_replacer}  {item["key"]}: {"{"}'
                 )
-                result.append(f'\n{prebracket_replacer}{"}"}')
+                result.append(
+                    f'{stylize(item["children"], replacer, spaces_count, level + 1)}'
+                    f'\n{prebracket_replacer}{"}"}'
+                )
 
             case 'added':
-                result.append(f'{prekey_replacer}+ {item["key"]}: ')
                 result.append(
-                    to_str(
-                        item['new_value'],
-                        replacer,
-                        spacesCount,
-                        level
-                    )
+                    f'{prekey_replacer}+ {item["key"]}: '
+                    f'{to_str(item["new_value"], replacer, spaces_count, level)}'
                 )
 
             case 'deleted':
-                result.append(f'{prekey_replacer}- {item["key"]}: ')
                 result.append(
-                    to_str(
-                        item['old_value'],
-                        replacer,
-                        spacesCount,
-                        level
-                    )
+                    f'{prekey_replacer}- {item["key"]}: '
+                    f'{to_str(item["old_value"], replacer, spaces_count, level)}'
                 )
 
             case 'unchanged':
-                result.append(f'{prekey_replacer}  {item["key"]}: ')
                 result.append(
-                    to_str(
-                        item['old_value'],
-                        replacer,
-                        spacesCount,
-                        level
-                    )
+                    f'{prekey_replacer}  {item["key"]}: '
+                    f'{to_str(item["old_value"], replacer, spaces_count, level)}'
                 )
 
             case 'updated':
-                result.append(f'{prekey_replacer}- {item["key"]}: ')
                 result.append(
-                    to_str(
-                        item['old_value'],
-                        replacer,
-                        spacesCount,
-                        level
-                    )
+                    f'{prekey_replacer}- {item["key"]}: '
+                    f'{to_str(item["old_value"], replacer, spaces_count, level)}'
                 )
-                result.append('\n')
 
-                result.append(prekey_replacer + '+ ' + item['key'] + ': ')
                 result.append(
-                    to_str(
-                        item['new_value'],
-                        replacer,
-                        spacesCount,
-                        level
-                    )
+                    f'{prekey_replacer}+ {item["key"]}: '
+                    f'{to_str(item["new_value"], replacer, spaces_count, level)}'
                 )
 
     if level == 1:
-        result.append('\n}')
-    return ''.join(result)
+        result.insert(0, '{')
+        result.append('}')
+    return '\n'.join(result)
 
 
-def to_str(argument, replacer=' ', spacesCount=1, level=0):
+def to_str(argument, replacer=' ', spaces_count=1, level=0):
     result = []
 
     if isinstance(argument, dict):
-        prekey_replacer = replacer * (spacesCount * level * 4 + 4)
-        prebracket_replacer = replacer * (spacesCount * level * 4)
+        prekey_replacer = replacer * (spaces_count * level * 4 + 4)
+        prebracket_replacer = replacer * (spaces_count * level * 4)
 
         result.append('{')
         for key in argument:
 
-            result.append(f'\n{prekey_replacer}{key}: ')
-
             result.append(
-                to_str(
-                    argument[key],
-                    replacer,
-                    spacesCount + 1,
-                    level
-                )
+                f'\n{prekey_replacer}{key}: '
+                f'{to_str(argument[key], replacer, spaces_count + 1, level)}'
             )
+
         result.append(f'\n{prebracket_replacer}{"}"}')
 
     if isinstance(argument, bool):
